0765d5d10cb8dc20cc5b955ca6b43b1915fb1384
Wolfe <wolfelienhardt@gmail.com>
2017-05-23 22:00:53 -0400

    Using vanilla react form

--- src/components/NameForm.js
+++ src/components/NameForm.js
@@ -1,26 +1,30 @@
 import React, { Component } from 'react';
 
 class NameForm extends Component {
+
   constructor(props) {
     super(props);
+    this.state = {value: '', output: ''};
 
-      this.state = {
-        name: '',
-        output: ''
-      }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
-  submitName(event) {
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
     event.preventDefault();
-    console.log(this.refs.name.value);
-    this.setState({output : this.refs.name.value})
-    this.refs.name.value = '';
-
+    console.log(this.state.name);
+    this.setState({output : this.state.value});
   }
+
   render() {
     return (
       <div>
-        <form onSubmit={this.submitName.bind(this)}>
-          <input type="text" ref="name"/>
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
           <input type="submit" value="Submit"/>
         </form>
         <p>{this.state.output}</p>
